99728f4428e0428a4fa7d0bb8ef83559aca6c280
Amol Prakash <amolprakash06@gmail.com>
2020-01-27 18:38:12 +0530

    Project Initial Commit

---
+++ Rivi Demo/Rivi Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rivi Demo
+//
+//  Created by Amol Prakash on 27/01/20.
+//  Copyright © 2020 Amol Prakash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
