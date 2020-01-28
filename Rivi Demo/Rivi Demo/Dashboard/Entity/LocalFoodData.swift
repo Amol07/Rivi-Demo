668847bedb5b312e7b58c5fafc1557f051490507
Amol Prakash <amolprakash06@gmail.com>
2020-01-28 18:07:19 +0530

    Created model architecture for json response.

--- Rivi Demo/Rivi Demo/Dashboard/Entity/LocalFoodData.swift
+++ Rivi Demo/Rivi Demo/Dashboard/Entity/LocalFoodData.swift
@@ -9,5 +9,32 @@
 import Foundation
 
 class LocalFoodData: Decodable  {
-    
+    var headerDetails: CardHeaderDetail?
+    var card: [Card]?
+}
+
+class CardHeaderDetail: Decodable {
+    var title: String?
+    var type: String?
+    var city: String?
+}
+
+class Card: Decodable {
+    var title: String?
+    var description: String?
+    var cardImage: String?
+    var cardNo: Int?
+    var cardDetails: CardDetail?
+}
+
+class CardDetail: Decodable {
+    var about: [String]?
+    var location: [Location]?
+    var dishes: [String]?
+    var images: [String]?
+}
+
+class Location: Decodable {
+    var name: String?
+    var distance: Float?
 }
